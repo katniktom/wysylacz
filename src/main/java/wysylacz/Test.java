81dcf5907accb648b1d58381c60c3119973e6e6b
Tomasz <Tomasz@Tomasz-Katnik>
2014-07-24 19:09:03 +0200

    v1

--- src/main/java/wysylacz/Test.java
+++ src/main/java/wysylacz/Test.java
@@ -1,36 +1,63 @@
 package wysylacz;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.SendFailedException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 public class Test {
+	public static void main(String[] args) throws MessagingException {
+		{
+			String host = "poczta.interia.pl";
+			String Password = "distefanoi";
+			String from = "katniktom@interia.pl";
+			
+			String toAddress = "llekutek@gmail.com";
+			String filename = "C:/Users/Tomasz/Desktop/appendix.txt";
+			// ("C:/Users/Tomasz/Desktop/CV-Tomasz K¹tnik - 2014_02_PL_PM.pdf");
+			// Get system properties
+			Properties props = System.getProperties();
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtps.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			Session session = Session.getInstance(props, null);
 
-	public static void main(String[] args) {
-		SimpleMailMessage message = new SimpleMailMessage();
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO, toAddress);
+			message.setSubject("Mail z zalcznikiem by TK");
+			BodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setText("To jest mail z zalacznikiem");
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(messageBodyPart);
+			messageBodyPart = new MimeBodyPart();
+			DataSource source = new FileDataSource(filename);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(filename);
+			multipart.addBodyPart(messageBodyPart);
+			message.setContent(multipart);
 
-		message.setFrom("llekutek@gmail.com");
-		message.setTo("llekutek@gmail.com");
-		message.setSubject("Javacodegeeks email test");
-		message.setText("Testing.. \n Hello Spring Email Sender");
-		JavaMailSenderImpl ms = new JavaMailSenderImpl();
-		ms.setHost("smtp.gmail.com");
-		ms.setPort(587);
-		ms.setUsername("llekutek");
-		ms.setPassword("nananana123");
-
-		Properties p = new Properties();
-		p.setProperty("mail.smtp.auth", "true");
-		p.setProperty("mail.smtp.starttls.enable", "true");
-		p.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com");
-
-		ms.setJavaMailProperties(p);
-		
-		ms.send(message);
-
+			try {
+				Transport tr = session.getTransport("smtps");
+				tr.connect(host, from, Password);
+				tr.sendMessage(message, message.getAllRecipients());
+				System.out.println("Mail Sent Successfully");
+				tr.close();
+			} catch (SendFailedException sfe) {
+				System.out.println(sfe);
+			}
+		}
 	}
 }
